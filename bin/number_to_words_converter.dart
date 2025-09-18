ce126d5bac25210f897ba24feef8d58248b50ec2
aghamyan <Arthur_aghamyan@edu.aua.am>
2025-09-18 19:59:27 +0400

    Initial commit: Dart homework solution

--- bin/number_to_words_converter.dart
+++ bin/number_to_words_converter.dart
@@ -1,5 +1,5 @@
 void main() {
-  int input = 1002; // change this number for testing
+  int input = 1002;
   print(numberToWords(input));
 }
 
@@ -7,49 +7,49 @@ String numberToWords(int number) {
   if (number == 0) return "zero";
 
   return [
-    if ((number ~/ 1000000) > 0) "${convertThreeDigits(number ~/ 1000000)} million",
-    if (((number ~/ 1000) % 1000) > 0) "${convertThreeDigits((number ~/ 1000) % 1000)} thousand",
-    if ((number % 1000) > 0) convertThreeDigits(number % 1000),
+    if ((number ~/ 1000000) > 0) "${ThreeDigits(number ~/ 1000000)} million",
+    if (((number ~/ 1000) % 1000) > 0) "${ThreeDigits((number ~/ 1000) % 1000)} thousand",
+    if ((number % 1000) > 0) ThreeDigits(number % 1000),
   ].join(" ").trim();
 }
 
-String convertThreeDigits(int num) {
+String ThreeDigits(int num) {
   if (num == 0) return "";
 
-  if (num < 100) return convertTwoDigits(num);
+  if (num < 100) return TwoDigits(num);
 
   int hundreds = num ~/ 100;
   int rest = num % 100;
 
   return [
-    "${units[hundreds]} hundred",
-    if (rest > 0) convertTwoDigits(rest),
+    "${OneToTen[hundreds]} hundred",
+    if (rest > 0) TwoDigits(rest),
   ].join(" ");
 }
 
-String convertTwoDigits(int num) {
-  if (num < 10) return units[num];
-  if (num < 20) return teens[num - 10];
+String TwoDigits(int num) {
+  if (num < 10) return OneToTen[num];
+  if (num < 20) return Tens[num - 10];
 
   int tensPlace = num ~/ 10;
   int onesPlace = num % 10;
 
   return [
-    tens[tensPlace],
-    if (onesPlace > 0) units[onesPlace]
+    Tens[tensPlace],
+    if (onesPlace > 0) OneToTen[onesPlace]
   ].join(" ");
 }
 
-final List<String> units = [
+final List<String> OneToTen = [
   "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
 ];
 
-final List<String> teens = [
+final List<String> TentoTweny = [
   "ten", "eleven", "twelve", "thirteen", "fourteen",
   "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
 ];
 
-final List<String> tens = [
+final List<String> Tens = [
   "", "", "twenty", "thirty", "forty", "fifty",
   "sixty", "seventy", "eighty", "ninety"
 ];
